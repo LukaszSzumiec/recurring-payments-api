561cbecda16108048ac87f19e658b632eb6b41cb
LukaszSzumiec <lukasz.d.szumiec@gmail.com>
2025-03-01 17:13:15 +0100

    Initial commit

---
+++ src/main/java/com/lukaszszumiec/recurring_payments_api/RecurringPaymentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszszumiec.recurring_payments_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecurringPaymentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecurringPaymentsApiApplication.class, args);
+	}
+
+}
